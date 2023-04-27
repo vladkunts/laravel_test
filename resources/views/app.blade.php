772b18a0b1bdddcf4ea806f8d33566f5d1a705b2
Vladimir Kuntsevich <vkuntsevich@gmail.com>
2023-04-27 15:25:42 +0300

    Initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
+    </head>
+<body>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    </div>
+</body>
+</html>
